d8601c111eb23983e7c35b825cfc72969e3c5444
jimjamjoyce <139064989+jimjamjoyce@users.noreply.github.com>
2023-09-07 22:37:21 +0100

    Update ato-app.py

--- interface/ato-app.py
+++ interface/ato-app.py
@@ -141,8 +141,9 @@ with col1:
 
 with col2:
     st.title("AND THEY'RE OFF!")
-    st.markdown('''##### <span style="color:black">A data-led approach to finding value in horse racing</span>
-                ''', unsafe_allow_html=True)
+    st.subheader('''##### <span style="color:black">A data-led approach to finding value in horse racing</span>
+                ''', unsafe_allow_html=True,
+                divider = "violet")
 
 tab_overview, tab_races, tab_analysis, tab_aboutus = st.tabs(["OVERVIEW",
                                                               "RACES",
